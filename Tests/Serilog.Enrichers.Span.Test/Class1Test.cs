0559e6cbd1a531bcd6f46942c96eafd6edd9a206
심승용 <seungyong.shim@mirero.co.kr>
2021-05-07 21:36:21 +0900

    Support AddTags

--- Tests/Serilog.Enrichers.Span.Test/Class1Test.cs
+++ Tests/Serilog.Enrichers.Span.Test/Class1Test.cs
@@ -1,6 +1,8 @@
 namespace Serilog.Enrichers.Span.Test
 {
+    using System.Diagnostics;
     using Serilog.Enrichers.Span;
+    using Serilog.Events;
     using Xunit;
 
     public class Class1Test
@@ -11,6 +13,48 @@ namespace Serilog.Enrichers.Span.Test
             var class1 = new ActivityEnricher();
 
             Assert.NotNull(class1);
+        }
+
+        [Fact]
+        public void AddTags()
+        {
+            LogEvent? evt = null;
+            var log = new LoggerConfiguration()
+                .Enrich.WithSpan()
+                .WriteTo.Sink(new DelegatingSink(e => evt = e))
+                .CreateLogger();
+
+            ActivitySource.AddActivityListener(new ActivityListener
+            {
+                ActivityStarted = activity => { },
+                ActivityStopped = activity => { },
+                ShouldListenTo = activitySource => true,
+                SampleUsingParentId = (ref ActivityCreationOptions<string> activityOptions) => ActivitySamplingResult.AllDataAndRecorded,
+                Sample = (ref ActivityCreationOptions<ActivityContext> activityOptions) => ActivitySamplingResult.AllDataAndRecorded,
+            });
+            var activitySource = new ActivitySource("Test");
+
+            
+            using (var activity = activitySource.StartActivity("AddTags"))
+            {
+                activity?.AddTag("Name", "Chris Shim");
+                activity?.AddTag("NullString", null);
+
+                log.Information(@"Has an EnvironmentUserName property with [domain\]userName");
+            }
+
+            Assert.NotNull(evt);
+
+            Assert.Equal("Chris Shim", evt?.Properties["Name"].LiteralValue());
+            Assert.Null(evt?.Properties["NullString"].LiteralValue());
+        }
+    }
+
+    internal static class Extensions
+    {
+        public static object LiteralValue(this LogEventPropertyValue @this)
+        {
+            return ((ScalarValue)@this).Value;
         }
     }
 }
