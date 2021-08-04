bae8ac5eae79f636f923ee278c11cc3df7da5f85
Kahbazi <a.kahbazi@gmail.com>
2021-08-04 17:10:55 +0430

    Add Activity tag enricher

---
+++ Source/Serilog.Enrichers.Span/ActivityTagEnricher.cs
@@ -0,0 +1,31 @@
+namespace Serilog.Enrichers.Span
+{
+    using System.Diagnostics;
+    using System.Linq;
+    using Serilog.Core;
+    using Serilog.Events;
+
+    /// <summary>
+    /// A log event enricher which adds tags from the current <see cref="Activity"/>.
+    /// </summary>
+    public class ActivityTagEnricher : ILogEventEnricher
+    {
+        private const string ActivityTags = "Attributes";
+
+        /// <summary>
+        /// Enrich the log event.
+        /// </summary>
+        /// <param name="logEvent">The log event to enrich.</param>
+        /// <param name="propertyFactory">Factory for creating new properties to add to the event.</param>
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var activity = Activity.Current;
+
+            if (activity is not null)
+            {
+                var tags = activity.Tags.Select(tag => new LogEventProperty(tag.Key, new ScalarValue(tag.Value)));
+                logEvent.AddPropertyIfAbsent(new LogEventProperty(ActivityTags, new StructureValue(tags)));
+            }
+        }
+    }
+}
